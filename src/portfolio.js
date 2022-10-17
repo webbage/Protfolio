3ecdcc8c361b53e2e0c6bfa83465e7628f8a2878
webbbg <113067650+webbage@users.noreply.github.com>
2022-10-18 02:52:43 +0300

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,61 @@
+const header = {
+    homepage: 'https://github.com/webbage',
+    title: 'HB.',
+  }
+  
+  const about = {
+    name: 'Hashim Bahaa',
+    role: 'React Developer',
+    description:
+      "I'm a junior developer and have the passion to learn more and more about web development, especially in the MERN Stack. My story starts with Angela Yu's bootcamp in July. From there, I found my passion for coding and building new apps every day.",
+    resume: './resume.pdf',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/hashim-bahaa-267a77253/',
+      github: 'https://github.com/webbage',
+    },
+  }
+  
+  const projects = [
+    {
+      name: 'Cat Facts',
+      description:
+        'Fun app with a background image and a cat sound that works with a button click and an API using React.js and axios.',
+      stack: ['React.js', 'API'],
+      sourceCode: 'https://github.com/webbage/Cat-Facts',
+      livePreview: 'https://random-z.herokuapp.com/',
+    },
+    {
+      name: 'Keeper',
+      description:
+        'A Google Keeper Clone built with react.js to keep your notes when you need them with advanced styling Give your mind a rest.',
+      stack: ['React.js'],
+      sourceCode: 'https://github.com/webbage/Keeper',
+      livePreview: 'http://webbage.github.io/Keeper',
+    },
+    {
+      name: 'Weather',
+      description:
+        'This API is unique in that it displays the city based on your input.',
+      stack: ['React.js', 'API'],
+      sourceCode: 'https://github.com/webbage/Weather',
+      livePreview: 'https://weather-widge.herokuapp.com/',
+    },
+  ]
+  
+  const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React.js',
+    'Node.js',
+    'MonogoDB',
+    'Express',
+    'Git hub',
+  ]
+  
+  const contact = {
+    email: 'HashimBahaa200@gmail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
+  
